4a551356afede4938573cf60b7dcc0f921444b4c
Gebreyesus_Kidanu <kidanu.gebreyesus@gmail.com>
2023-12-29 12:34:33 +0300

    Modified the text content

--- meal_purchased.py
+++ meal_purchased.py
@@ -1,7 +1,9 @@
+
+print("Welcome to the tip calculator!")
 charge_food=float(input('Please enter the charge for the food : '))
 tip_amount=(charge_food*18/100) # Tip amount in %
 sales_tax=(charge_food*7/100) # sales tax amount in %
 Grand_total=(charge_food + tip_amount + sales_tax) #display the charge of the food plus tip and sales tax.
-print('Tip in $ = ',tip_amount)
-print('Sales tax  in $ =',sales_tax)
-print('The total_price in $= ', Grand_total)
+print(f"Tip paid: ${tip_amount}")
+print(f"The total Sales tax: ${sales_tax}")
+print(f"The grand total amount paid: ${Grand_total}")
